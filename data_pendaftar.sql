9746996b567a47086edb58e08e055fd3a77d010b
Rhe <78008101+natsumiomika@users.noreply.github.com>
2021-02-15 19:14:56 +0700

    Add files via upload

--- data_pendaftar.sql
+++ data_pendaftar.sql
@@ -1,19 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 04, 2021 at 06:33 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sttb1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_pendaftar`
+--
+
 CREATE TABLE `data_pendaftar` (
-  `id` int(11) NOT NULL,
-  `username` varchar(25) NOT NULL,
+  `id` int(20) NOT NULL,
+  `nama_depan` varchar(50) NOT NULL,
+  `nama_belakang` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `data_pendaftar` (`id`, `username`, `email`, `password`) VALUES
-(NULL, 'rifkiadriansyah24@gmail.com', 'rhealiz', SHA1('12345678'));
+--
+-- Dumping data for table `data_pendaftar`
+--
 
+INSERT INTO `data_pendaftar` (`id`, `nama_depan`, `nama_belakang`, `email`, `username`, `password`) VALUES
+(14, 'sani', 'hamdhani', 'asd', 'ada', 'afafa');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_pendaftar`
+--
 ALTER TABLE `data_pendaftar`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_pendaftar`
+--
 ALTER TABLE `data_pendaftar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
